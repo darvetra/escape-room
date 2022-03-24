170c08a8202caf6136f73f503335980b03843983
Darvetra <darvetra13@gmail.com>
2022-03-24 21:55:24 +0300

    добавляет моковые данные

---
+++ src/types/quest.ts
@@ -0,0 +1,13 @@
+export type QuestType = {
+  id: number,
+  title: string,
+  description: string,
+  previewImg: string,
+  coverImg: string,
+  type: string,
+  level: string,
+  peopleCount: number[],
+  duration: number
+}
+
+export type QuestsType = QuestType[];
