e2f9af67988095362dd4f5436c94b4e3dcf6c187
Darvetra <darvetra13@gmail.com>
2022-03-28 20:41:09 +0300

    подключает redux к проекту

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
 import App from './components/app/app';
+import { reducer } from './store/reducer';
 
+// моки
 import { questsList } from './mocks/quests';
-
 const quest = questsList[0];
+
+const store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App quests={questsList} quest={quest} />
+    <Provider store = {store}>
+      <App quests={questsList} quest={quest} />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'));
